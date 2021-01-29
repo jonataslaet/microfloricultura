54fe1decbcfade1bb7d66588ebf868756f7294e9
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-01-29 06:06:00 -0300

    Adicionado o eureka-server também foi feita integração com os microservicos loja e fornecedor também foi melhorado o RestTemplate

--- loja/src/main/java/br/com/jonataslaet/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/jonataslaet/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.jonataslaet.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,12 @@ import br.com.jonataslaet.microservice.loja.controller.dto.InfoFornecedor;
 @Service
 public class CompraService {
 	
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedor> exchange = client.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedor.class);
+		ResponseEntity<InfoFornecedor> exchange = client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedor.class);
 		System.out.println(exchange.getBody().getEndereco());
 	}
 
