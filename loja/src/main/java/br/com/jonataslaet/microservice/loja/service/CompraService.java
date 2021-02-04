271e7aa51b1023e55f09238e8cb2e2e42e2c6da1
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-03 21:34:05 -0300

    Adicionado o hystrix no microserviço da loja a princípio, ele foi chamado apenas no método realizaCompra

--- loja/src/main/java/br/com/jonataslaet/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/jonataslaet/microservice/loja/service/CompraService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.jonataslaet.microservice.loja.client.FornecedorClient;
 import br.com.jonataslaet.microservice.loja.controller.CompraDTO;
 import br.com.jonataslaet.microservice.loja.controller.dto.InfoFornecedorDTO;
@@ -18,6 +20,7 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallBack")
 	public Compra realizaCompra(CompraDTO compra) {
 		String estado = compra.getEndereco().getEstado();
 		LOG.info("Buscando informações do fornecedor do estado {}", estado);
@@ -34,6 +37,12 @@ public class CompraService {
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
 		
 		return compraSalva;
+	}
+	
+	public Compra realizaCompraFallBack(CompraDTO compra) {
+		Compra compraFallBack = new Compra();
+		compraFallBack.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallBack;
 	}
 
 }
